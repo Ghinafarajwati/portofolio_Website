3f932b0189425ca9b6a596c5684a1c9ebdb45b1a
ghinafarajwati <g.farajwati@yahoo.com>
2024-02-03 23:11:56 +0700

    new techstack

--- src/pages/techStack.jsx
+++ src/pages/techStack.jsx
@@ -13,7 +13,7 @@ const Tech = () => {
           transition={{duration: 0.5}}
           variants={{
             hidden: {opacity: 0, y: -50},
-            visible: {opacity: 1, y: 0}
+            visible: {opacity: 1, y: 0},
           }}>
             <h2 className='text-center text-4xl font-bold text-white mt-14'>Tech Stack</h2>
             <p className='text-center text-mg text-white mt-5'>Here are some skills I have diligently cultivated in the process of acquiring proficiency in programming.</p>
@@ -31,14 +31,14 @@ const Tech = () => {
             <div className='flex flex-row md:justify-start'>
               <h3 className='text-start text-xl md:mx-[5rem] mx-[8rem] mt-[2rem] border-b border-blue'>OTHERS</h3>
             </div>
-            <div className='grid xl:grid-cols-10 md:grid-cols-7 xs:grid-cols-7 gap-6 text-center py-8 px-12 sm:px-0 mx-[5rem] mt-[-1rem]'>
-            {TechData.slice(12,15).map(({id, image, title}) => (
-                <div key={id} className='hover:scale-105'>
-                <img src={image} alt='others'></img>
-                <p>{title}</p>
+              <div className='grid xl:grid-cols-10 md:grid-cols-7 xs:grid-cols-7 gap-6 text-center py-8 px-12 sm:px-0 mx-[5rem] mt-[-1rem]'>
+              {TechData.slice(12,15).map(({id, image, title}) => (
+                  <div key={id} className='hover:scale-105'>
+                  <img src={image} alt='others'></img>
+                  <p>{title}</p>
+                </div>
+              ))}
               </div>
-            ))}
-            </div>
           </motion.div>
       </div>
     </section>
